cd238c3f6d04d1a9f9366ef34c2f54e004260e6d
cy <cyberschool.moscow@gmail.com>
2022-06-24 10:20:22 +0300

    1) Sign in option added

--- app/src/main/java/com/cy/myappauth/MainActivity.java
+++ app/src/main/java/com/cy/myappauth/MainActivity.java
@@ -1,14 +1,172 @@
 package com.cy.myappauth;
 
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int GOOGLE_SIGN_IN = 123;
+    FirebaseAuth mAuth;
+    Button btn_login, btn_logout,go_btn;
+    TextView text;
+    ImageView image;
+    ProgressBar progressBar;
+    GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_login = findViewById(R.id.login_btn);
+        btn_logout = findViewById(R.id.logout_btn);
+        go_btn = findViewById(R.id.login_btn2);
+
+        text = findViewById(R.id.text);
+        image = findViewById(R.id.image);
+        progressBar = findViewById(R.id.progress_circular);
+        progressBar.setVisibility(View.INVISIBLE);
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("403111546035-8udtqr0c869t2oa2u1dm0r3ev4mfb4gb.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        btn_login.setOnClickListener(v -> SignInGoogle());
+        btn_logout.setOnClickListener(v -> Logout());
+        go_btn.setOnClickListener(v -> GoNext());
+
+        if (mAuth.getCurrentUser() != null) {
+            FirebaseUser user = mAuth.getCurrentUser();
+            updateUI(user);
+        }
+    }
+
+    private void GoNext() {
+        TestAct();
+        Intent intent=new Intent(this,MainMenuActivity.class);
+        startActivity(intent);
+    }
+
+
+    DatabaseReference reference;
+    FirebaseFirestore db ;
+    private void TestAct() {
+        String userTofind=userDataTest.userName;
+    }
+
+    public void SignInGoogle() {
+        progressBar.setVisibility(View.VISIBLE);
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, GOOGLE_SIGN_IN);
+       //? registerForActivityResult(signInIntent,GOOGLE_SIGN_IN);
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d("TAG", "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+//                        progressBar.setVisibility(View.INVISIBLE);
+
+                        Log.d("TAG", "signInWithCredential:success");
+
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        updateUI(user);
+                        progressBar.setVisibility(View.INVISIBLE);
+                    } else {
+//                        progressBar.setVisibility(View.INVISIBLE);
+
+                        Log.w("TAG", "signInWithCredential:failure", task.getException());
+
+//                        Toast.makeText(MainActivity.this, "Authentication failed.",
+//                                Toast.LENGTH_SHORT).show();
+                        updateUI(null);
+                        progressBar.setVisibility(View.INVISIBLE);
+                    }
+                });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GOOGLE_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                if (account != null) firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                Log.w("TAG", "Google sign in failed", e);    Toast.makeText(MainActivity.this, "Authentication failed.",
+                        Toast.LENGTH_LONG).show();
+                //progressBar.setVisibility(View.INVISIBLE);
+            }
+        }
+    }
+
+    private void updateUI(FirebaseUser user) {
+        //progressBar.setVisibility(View.INVISIBLE);
+        if (user != null) {
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            String photo = String.valueOf(user.getPhotoUrl());
+
+            userDataTest.photo=photo;
+            userDataTest.userName=user.getEmail();
+
+            text.append("Info : \n");
+            text.append(name + "\n");
+            text.append(email);
+            Picasso.get().load(photo).into(image);
+            btn_logout.setVisibility(View.VISIBLE);
+            go_btn.setVisibility(View.VISIBLE);
+
+            btn_login.setVisibility(View.INVISIBLE);
+
+
+        } else {
+            text.setText("Firebase Login \n");
+            Picasso.get().load(R.drawable.ic_firebase_logo).into(image);
+            btn_logout.setVisibility(View.INVISIBLE);
+
+            go_btn.setVisibility(View.INVISIBLE);
+            btn_login.setVisibility(View.VISIBLE);  Toast.makeText(MainActivity.this, "Authentication failed.",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void Logout() {
+        FirebaseAuth.getInstance().signOut();
+        mGoogleSignInClient.signOut().addOnCompleteListener(this,
+                task -> updateUI(null));
     }
 }
---
+++ app/src/main/java/com/cy/myappauth/userDataTest.java
@@ -0,0 +1,18 @@
+package com.cy.myappauth;
+
+import java.util.ArrayList;
+
+public class userDataTest {
+    public static   String photo;
+    public static String userName;
+
+    //first look at this
+    public static String userFirstAcName;
+    public static String usref;
+    public static String usDishref;
+    public  static  boolean isNewUser,isSecondUser;
+    public  static  boolean isEditing;
+    public static Float sum=0f;
+    public static ArrayList<Integer> curN;//=0;
+    //public static DishItem di;
+}
